5511f4c9c87372c1d2fc0c9d64e324bc24a444e0
Alehandro Persaud <andrew041994@gmail.com>
2019-12-30 17:48:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-    hash[key] ? hash[key] : hash[key] =1 
-    hash 
+    hash[key] ? hash[key] : hash[key] =1
+    hash
 end
